1a6b63a6d0ac2e702af6f4630e64287780e21778
steve <steve0639@cchntek.com>
2020-03-13 12:02:12 +0800

    [update] new animeJS

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,7 +1,85 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import styled from "styled-components";
+import anime from "animejs";
 
+
+const StyleHome = styled.div`
+  width:1000px;
+  margin: 0 auto;
+  .title {
+    padding:10px 0;
+    font-weight:bold;
+    font-size:20px;
+    color:#757575;
+    span {
+      font-size:24px;
+      color:#333333;
+    }
+  }
+  .animeBox {
+    width:80px;
+    height:80px;
+    background-color:"#E32636";
+  }
+`
 export default ({location: { pathname }})=> {
+  const locationName = pathname.split("/");
+  const animeBox = useRef(null);
+  useEffect(()=>{
+    anime.timeline({
+      easing: 'easeInOutQuad',
+      duration: 800,
+      loop: true
+    })
+    .add({
+      targets: animeBox.current,
+      translateX: [0,200],
+      borderRadius: ['0%', '50%'],
+      scale: 0.8,
+      backgroundColor:'#00BFFF'
+    })
+    .add({
+      targets: animeBox.current,
+      translateY: [0,150],
+      borderRadius: ['50%', '0%'],
+      scale: 1,
+      backgroundColor:'#000'
+    })
+    .add({
+      targets: animeBox.current,
+      translateX: [200,0],
+      borderRadius: ['0%', '50%'],
+      scale: 0.8,
+      backgroundColor:'#66FF00'
+    })
+    .add({
+      targets: animeBox.current,
+      translateY: [150,0],
+      borderRadius: ['50%', '0%'],
+      scale: 1,
+      backgroundColor:"#E32636"
+    })
+  },[animeBox])
   return (
-    <div>this is {pathname}</div>
-  )
+    <StyleHome>
+      <div className="title">this is <span>{locationName[1].toUpperCase()}</span></div>
+      <div className="animeBox" 
+        ref={animeBox}
+        // onMouseOver={()=>{
+        //   anime({
+        //     targets: animeBox.current,
+        //     translateX: [0,200],
+        //     duration: 1000,
+        //   });
+        // }}
+        // onMouseLeave={()=>{
+        //   anime({
+        //     targets: animeBox.current,
+        //     translateX: [200,0],
+        //     duration: 1000,
+        //   });
+        // }}
+      />
+    </StyleHome>
+  );
 };
--- src/components/Pages/Other.js
+++ src/components/Pages/Other.js
@@ -1,7 +1,25 @@
 import React from "react";
+import styled from "styled-components";
 
+const StyleHome = styled.div`
+  width:1000px;
+  margin: 0 auto;
+  .title {
+    padding:10px 0;
+    font-weight:bold;
+    font-size:20px;
+    color:#757575;
+    span {
+      font-size:24px;
+      color:#333333;
+    }
+  }
+`
 export default ({location: { pathname }})=> {
+  const locationName = pathname.split("/");
   return (
-    <div>this is {pathname}</div>
-  )
+    <StyleHome>
+      <div className="title">this is <span>{locationName[1].toUpperCase()}</span></div>
+    </StyleHome>
+  );
 };
