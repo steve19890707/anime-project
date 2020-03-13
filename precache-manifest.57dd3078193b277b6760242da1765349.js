19132e68fe22240e58901ffffd145f5d631ec2ad
steve <steve0639@cchntek.com>
2020-03-13 09:54:20 +0800

    Updates

---
+++ precache-manifest.57dd3078193b277b6760242da1765349.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad74158010f64938e007ebbb3135186e",
+    "url": "/anime-project/index.html"
+  },
+  {
+    "revision": "30d0b4aaaab1d99c64a8",
+    "url": "/anime-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "644992cf89e1fb95a8d6",
+    "url": "/anime-project/static/js/2.f8384d6e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/anime-project/static/js/2.f8384d6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30d0b4aaaab1d99c64a8",
+    "url": "/anime-project/static/js/main.94e5917f.chunk.js"
+  },
+  {
+    "revision": "f1da8d5dbe4c3136a88b",
+    "url": "/anime-project/static/js/runtime-main.e11684ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/anime-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
