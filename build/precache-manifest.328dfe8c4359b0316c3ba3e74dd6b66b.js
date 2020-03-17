fab3f07ccd910caf5c262b8efd05b7a5690f11ba
steve <steve0639@cchntek.com>
2020-03-17 14:17:06 +0800

    [update] new build

---
+++ build/precache-manifest.328dfe8c4359b0316c3ba3e74dd6b66b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e06376ac8a85b8d2554f3ec94a699a50",
+    "url": "/anime-project/index.html"
+  },
+  {
+    "revision": "603aee3e615b37ea9b71",
+    "url": "/anime-project/static/js/2.43907b5d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/anime-project/static/js/2.43907b5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "358ab26cbad4a5823abc",
+    "url": "/anime-project/static/js/main.c9763128.chunk.js"
+  },
+  {
+    "revision": "f1da8d5dbe4c3136a88b",
+    "url": "/anime-project/static/js/runtime-main.e11684ef.js"
+  }
+]);
